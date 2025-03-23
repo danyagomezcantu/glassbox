a42200fc74d7fd48bb5bc29fbac741bac7483f13
Danya Gómez Cantú <111596636+danyagomezcantu@users.noreply.github.com>
2025-03-23 09:40:57 -0600

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,8 @@
+def classify_sentiment(text):
+    if "good" in text.lower() or "great" in text.lower():
+        return "positive"
+    elif "bad" in text.lower() or "hate" in text.lower():
+        return "negative"
+    else:
+        return "neutral"
 
