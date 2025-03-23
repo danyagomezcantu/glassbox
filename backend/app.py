c6b6acf29da7fe030bba395239a802254c96bb9e
Danya Gómez Cantú <111596636+danyagomezcantu@users.noreply.github.com>
2025-03-23 09:40:18 -0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
+from flask import Flask, request, jsonify
+from tee_emulator import run_private_inference
 
+app = Flask(__name__)
+
+@app.route('/inference', methods=['POST'])
+def inference():
+    user_input = request.json.get("message")
+    result = run_private_inference(user_input)
+    return jsonify({"result": result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
